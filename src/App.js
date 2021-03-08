b67a8b90853f944172350d4e2ff5d7e58dc0d5fc
yuki-misumi-sbisecsol <yuki.misumi@sbisecsol.com>
2021-03-08 22:18:23 +0900

    added training registration

--- src/App.js
+++ src/App.js
@@ -1,47 +1,91 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            trainings: [],
-            achievedSet: new Set()
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      trainings: [],
+      achievedSet: new Set(),
+      trainingName: "",
+      trainingVelocity: 0,
+      trainingUnit: "",
+    };
+  }
 
-    componentDidMount() {
-        axios.get(`${process.env.REACT_APP_BACKEND_URL}/index`)
-            .then((res) => {
-                this.setState({
-                    trainings: res.data.trainings,
-                    achievedSet: res.data.achievedSet
-                });
-            });
-    }
+  getIndexData() {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/index`).then((res) => {
+      this.setState({
+        trainings: res.data.trainings,
+        achievedSet: res.data.achievedSet,
+      });
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>NETERO</h1>
-                <p>Welcome to Netero</p>
-                <ul>
-                    {this.state.trainings.map(tr =>
-                        <li key={tr.id}>{tr.name} {tr.velocity} {tr.unit}</li>
-                    )}
-                </ul>
-                <input type="text" name="name" />
-                <br />
-                <input type="number" name="velocity" />
-                <br />
-                <input type="text" name="unit" />
-                <br />
-                <button type="button">登録</button>
-                <br />
-                <button type="button">ログアウトする</button>
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.getIndexData();
+  }
+
+  handleSubmit(e) {
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/training`, {
+        name: this.state.trainingName,
+        velocity: this.state.trainingVelocity,
+        unit: this.state.trainingUnit,
+      })
+      .then((res) => {
+        if (res.status !== 200) return;
+        this.getIndexData();
+      });
+    e.preventDefault();
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>NETERO</h1>
+        <p>Welcome to Netero</p>
+        <ul>
+          {this.state.trainings.map((tr) => (
+            <li key={tr.id}>
+              {tr.name} {tr.velocity} {tr.unit}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            name="trainingName"
+            value={this.state.trainingName}
+            onChange={this.handleChange.bind(this)}
+          />
+          <br />
+          <input
+            type="number"
+            name="trainingVelocity"
+            value={this.state.trainingVelocity}
+            onChange={this.handleChange.bind(this)}
+          />
+          <br />
+          <input
+            type="text"
+            name="trainingUnit"
+            value={this.state.trainingUnit}
+            onChange={this.handleChange.bind(this)}
+          />
+          <br />
+          <input type="submit" value="登録" />
+        </form>
+        <button type="button">ログアウトする</button>
+      </div>
+    );
+  }
 }
 
 export default App;
